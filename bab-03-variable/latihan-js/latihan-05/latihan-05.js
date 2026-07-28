48bbb797f6f32727f89ba61bcf9fe91f7e3066a0
Miyyus.445 <miyyus.445@gmail.com>
2026-07-28 14:13:23 +0700

    Menyelesaikan Bab 3 - Latihan 5

--- bab-03-variable/latihan-js/latihan-05/latihan-05.js
+++ bab-03-variable/latihan-js/latihan-05/latihan-05.js
@@ -1,2 +1,10 @@
 /* Latihan-05 */
 
+let nilaiMatematika = 80
+let nilaiIPA = 95
+let nilaiBahasaIndonesia = 90
+
+const jumlahPelajaran = 3
+const rataRata = (nilaiMatematika + nilaiIPA + nilaiBahasaIndonesia) / jumlahPelajaran
+
+console.log(`Rata-rata nilai adalah ${rataRata}`);
