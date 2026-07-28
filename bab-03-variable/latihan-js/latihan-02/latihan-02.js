b56c4f1df9d0a5a87138fb02109e8a647d407816
Miyyus.445 <miyyus.445@gmail.com>
2026-07-28 11:12:44 +0700

    Menyelesaikan Bab 3 - Latihan 2

--- bab-03-variable/latihan-js/latihan-02/latihan-02.js
+++ bab-03-variable/latihan-js/latihan-02/latihan-02.js
@@ -1,5 +1,14 @@
 /* Latihan-02 */
 
 const judul = 'Saldo';
-let saldoAwal = '100000';
-l
+let saldoAwal = 100000;
+let saldoAkhir = 50000;
+
+// judul-Saldo
+console.log(judul)
+// saldoAwal
+console.log(saldoAwal);
+// saldoAkhir
+console.log(saldoAkhir);
+// total saldo
+console.log('Saldo Awal : ' + saldoAwal + ' ' + 'Saldo Akhir : ' + (saldoAwal + saldoAkhir));
