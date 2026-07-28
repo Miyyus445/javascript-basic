39ccc62240eacf6c6c212de3a1464d775e87dddd
Miyyus.445 <miyyus.445@gmail.com>
2026-07-28 14:34:18 +0700

    Menyelesaikan Bab 3 - Challenge-01

---
+++ bab-03-variable/latihan-js/challenge-01/challenge-01.js
@@ -0,0 +1,19 @@
+/* Challenge-01 */
+
+const struk = '========== STRUK =========='
+const produk = 'Mouse'
+const hargaProduk = 165000
+const PAJAK_INDO = 0.12
+
+let jumlahDibeli = 2
+let totalSebelumPajak = hargaProduk + jumlahDibeli
+let pajak = totalSebelumPajak * PAJAK_INDO
+let totalAkhir = totalSebelumPajak + pajak
+
+console.log(struk);
+console.log(`Produk        : ${produk}`);
+console.log(`Harga         : Rp.${hargaProduk}`);
+console.log(`Jumlah Dibeli : ${jumlahDibeli}`);
+console.log(`Subtotal      : Rp.${totalSebelumPajak}`);
+console.log(`Pajak 12%     : Rp.${pajak}`);
+console.log(`Total         : Rp.${totalAkhir}`);
