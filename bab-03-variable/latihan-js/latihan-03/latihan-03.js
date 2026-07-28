cb82e072835cd26ab217a69fb5332baa6607bee9
Miyyus.445 <miyyus.445@gmail.com>
2026-07-28 13:26:29 +0700

    Menyelesaikan Bab 3 - Latihan 3

---
+++ bab-03-variable/latihan-js/latihan-03/latihan-03.js
@@ -0,0 +1,14 @@
+/* Latihan-03 */
+
+const namaProduk = 'Ram DDR4 8GB'
+const hargaSatuan = 200000
+const jumlahDibeli = 4
+
+// Nama Produk
+console.log(namaProduk);
+// Harga Satuan
+console.log('Harga Satuan' + ' ' + hargaSatuan);
+// Jumlah Di Beli
+console.log('Jumlah Di Beli' + ' ' + jumlahDibeli);
+// Total
+console.log('Total = ' + (hargaSatuan * jumlahDibeli));
