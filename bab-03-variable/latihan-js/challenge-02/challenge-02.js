c760b1ba1029bc5bb71bcde0b5e56e673d2384c9
Miyyus.445 <miyyus.445@gmail.com>
2026-07-28 14:45:59 +0700

    Menyelesaikan Bab 3 - Challenge-02

---
+++ bab-03-variable/latihan-js/challenge-02/challenge-02.js
@@ -0,0 +1,17 @@
+/* Challenge-02 */
+
+const profil = '========== PROFIL =========='
+const myName = 'Azmi Abiyyu Sakha'
+let umurSaya = 19
+const asalKota = 'Bekasi'
+const emailSaya = 'miyyus.445@gmail.com'
+const hobySaya = 'Photography, Videography, Editing, Teknologi & Otomotif'
+const citaCita = 'FullStuck Development'
+
+console.log(profil);
+console.log(`Nama       : ${myName}`);
+console.log(`Umur       : ${umurSaya}`);
+console.log(`Asal Kota  : ${asalKota}`);
+console.log(`Email      : ${emailSaya}`);
+console.log(`Hobi       : ${hobySaya}`);
+console.log(`Cita-cita  : ${citaCita}`);
