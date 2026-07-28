33ca5dcb62b314a9ecf743c14d92174eb52cc7f0
Miyyus.445 <miyyus.445@gmail.com>
2026-07-28 10:49:26 +0700

    Menyelesaikan Bab 3 - Latihan 1

---
+++ bab-03-variable/latihan-js/latihan-01/latihan-01.js
@@ -0,0 +1,18 @@
+/* Latihan-01 js */
+
+const outPut = '===== BIODATA ====='
+const myName = 'Nama : Azmi Abiyyu Sakha';
+let umurSaya = 'Umur : 17';
+const kota = 'Kota : Bekasi';
+let pekerjaan = 'Pekerjaan : Pelajar';
+
+// output
+console.log(outPut);
+// myName
+console.log(myName);
+// umurSaya
+console.log(umurSaya);
+// kota
+console.log(kota);
+// pekerjaan
+console.log(pekerjaan);
