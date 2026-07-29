b8c1fd5290db956fc00e266b05f1d35919fd2840
Miyyus.445 <miyyus.445@gmail.com>
2026-07-29 14:21:51 +0700

    Menyelesaikan Bab 4 - Bonus

---
+++ bab-04-tipedata/soal/bonus/bonus.js
@@ -0,0 +1,15 @@
+/* Bonus */
+
+const namaKelas = 'C';
+const jumlahSiswa = 9;
+let ketuaKelas = 'Rhadit';
+const daftarMapel = [
+    'JavaScript',
+    'HTML',
+    'CSS',
+]
+
+console.log(`Nama Kelas     : ${namaKelas}`);
+console.log(`Jumlah Siswa   : ${jumlahSiswa}`);
+console.log(`Ketua Kelas    : ${ketuaKelas}`);
+console.log(`Daftar Mapel   : ${daftarMapel[1]}, ${daftarMapel[2]}, ${daftarMapel[0]}`);
