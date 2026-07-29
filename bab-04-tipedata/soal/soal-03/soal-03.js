6bad8fd9bbb3b2c98566a008e9853ad0f5ee500e
Miyyus.445 <miyyus.445@gmail.com>
2026-07-29 11:31:03 +0700

    Menyelesaikan Bab 4 - Soal 3

---
+++ bab-04-tipedata/soal/soal-03/soal-03.js
@@ -0,0 +1,15 @@
+/* Soal-03 */
+
+let dataUser = {
+    namaSaya: 'Azmi',
+    umurSaya: 19,
+    sekolahSaya: 'Pondok IT',
+    jurusanSaya: 'Programer',
+    aktif: true
+}
+
+console.log(dataUser.namaSaya);
+console.log(dataUser.umurSaya);
+console.log(dataUser.sekolahSaya);
+console.log(dataUser.jurusanSaya);
+console.log(dataUser.aktif);
