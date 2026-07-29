8a574eb0eaf30e29febae79552271a3db5ddfdd8
Miyyus.445 <miyyus.445@gmail.com>
2026-07-29 10:56:43 +0700

    Menyelesaikan Bab 4 - Soal 1

---
+++ bab-04-tipedata/soal/soal-01/soal-01.js
@@ -0,0 +1,13 @@
+/* Soal-01 */
+
+const namaSaya = 'Azmi';
+const umurSaya = 19;
+const sudahLulus = true;
+const alamatSaya = null;
+const nomorTelepon = undefined;
+
+console.log(namaSaya);
+console.log(umurSaya);
+console.log(sudahLulus);
+console.log(alamatSaya);
+console.log(nomorTelepon);
