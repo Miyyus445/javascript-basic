5b2e712313899f1acfab5cd05e3f94e15e43dff0
Miyyus.445 <miyyus.445@gmail.com>
2026-07-29 11:11:12 +0700

    Menyelesaikan Bab 4 - Soal 2

---
+++ bab-04-tipedata/soal/soal-02/soal-02.js
@@ -0,0 +1,12 @@
+/* Soal-02 */
+
+const hobi = [
+    "Membaca",
+    "Fotografi",
+    "Videografi",
+    "Otomotif",
+    "Teknologi",
+    "Olahraga",
+];
+
+console.log(`Hobi saya ${hobi[1]}, ${hobi[2]}, ${hobi[0]}, ${hobi[5]}, ${hobi[4]}, dan ${hobi[3]}`);
