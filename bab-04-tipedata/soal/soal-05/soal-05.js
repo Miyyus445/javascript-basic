aa45bd2c62728c9bb1f8a5467fb648b41c91b43f
Miyyus.445 <miyyus.445@gmail.com>
2026-07-29 14:06:10 +0700

    Menyelesaikan Bab 4 - Soal 5

---
+++ bab-04-tipedata/soal/soal-05/soal-05.js
@@ -0,0 +1,9 @@
+/* Soal-05 */
+
+console.log(typeof "Hello");        //string
+console.log(typeof 100);            //number
+console.log(typeof true);           //boolean
+console.log(typeof null);           //null
+console.log(typeof []);             //array
+console.log(typeof {});             //objek
+console.log(typeof undefined);      //undefined
