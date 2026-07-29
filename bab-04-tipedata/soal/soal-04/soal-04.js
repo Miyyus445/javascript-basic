0d139887c1c45a09a1f256acdbd0bdf7be1bf9ff
Miyyus.445 <miyyus.445@gmail.com>
2026-07-29 13:48:19 +0700

    Menyelesaikan Bab 4 - Soal 4

---
+++ bab-04-tipedata/soal/soal-04/soal-04.js
@@ -0,0 +1,31 @@
+/* Soal-04 */
+
+const namaSaya = "Azmi";
+let umurSaya = 19;
+const sudahLulus = true;
+const alamatSaya = null;
+const nomorTelepon = undefined;
+const hobi = [
+  "Membaca",
+  "Fotografi",
+  "Videografi",
+  "Otomotif",
+  "Teknologi",
+  "Olahraga",
+];
+let profil = {
+  namaSaya: "Azmi",
+  umurSaya: 19,
+  sekolahSaya: "Pondok IT",
+  jurusanSaya: "Programer",
+  status: "aktif",
+};
+
+//typeof
+console.log(typeof namaSaya);       //string
+console.log(typeof umurSaya);       //number
+console.log(typeof sudahLulus);     //boolean
+console.log(typeof alamatSaya);     //null
+console.log(typeof nomorTelepon);   //undefined
+console.log(typeof hobi);           //array
+console.log(typeof dataUser);       //objek
