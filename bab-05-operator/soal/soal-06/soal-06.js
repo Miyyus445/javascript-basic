9d6b9f3913d040376676e29e3d22fe6ee4cb4526
Miyyus.445 <miyyus.445@gmail.com>
2026-07-30 13:39:26 +0700

    Menyelesaikan Bab 5 - Soal 7

---
+++ bab-05-operator/soal/soal-06/soal-06.js
@@ -0,0 +1,8 @@
+/* Soal-06 */
+
+console.log(10 > 5);        //true
+console.log(10 < 5);        //false
+console.log(10 >= 10);      //true
+console.log(5 <= 3);        //false
+console.log(8 != 8);        //false
+console.log(12 !== "12");   //false => yg bener true
---
+++ bab-05-operator/soal/soal-07/soal-07.js
@@ -0,0 +1,11 @@
+/* Soal-07 */
+
+console.log(10 == "10");            //true
+console.log(10 === "10");           //false
+console.log(false == 0);            //true  
+console.log(false === 0);           //false
+console.log(null == undefined);     //true
+console.log(null === undefined);    //false
+
+//karena == (si santai) operator ini cuma peduli sama nilai nya, dan ga peduli tipe data nya beda apa nggak
+//yang satu lagi === (si ketat) operator ini ketat banget karena nilai dan tipe data nya harus sama 
