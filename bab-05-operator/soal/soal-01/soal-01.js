3b4e6e75d316f7f17037dac97b476243d5ad825a
Miyyus.445 <miyyus.445@gmail.com>
2026-07-30 10:58:05 +0700

    Menyelesaikan Bab 5 - Soal 1

---
+++ bab-05-operator/soal/soal-01/soal-01.js
@@ -0,0 +1,11 @@
+/* Soal-01 */
+
+const panjang = 15;
+const lebar = 8;
+
+// Jawaban:
+
+// Luas
+console.log(`Luas       : ${panjang * lebar}`);
+// Keliling
+console.log(`Keliling   : ${2 * (panjang + lebar)}`);
