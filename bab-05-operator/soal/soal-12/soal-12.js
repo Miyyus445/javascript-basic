fceaf69276fde2d942787b1111b3940fb0243116
Miyyus.445 <miyyus.445@gmail.com>
2026-07-30 14:35:25 +0700

    Menyelesaikan Bab 5 - Soal 12

---
+++ bab-05-operator/soal/soal-12/soal-12.js
@@ -0,0 +1,6 @@
+/* Soal-12 */
+
+let umur = "18";
+
+console.log(umur > 17);     //true karena pakai Perbandingan relasional yang sifat nya tidak peduli pada jenis data
+console.log(umur === 18);   //false karena === (si ketat) operator ini ketat banget karena nilai dan tipe data nya harus sama 
