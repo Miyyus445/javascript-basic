64229d6a269b8f112ea825df9e4641c619f024f7
Miyyus.445 <miyyus.445@gmail.com>
2026-07-30 11:34:03 +0700

    Menyelesaikan Bab 5 - Soal 5

---
+++ bab-05-operator/soal/soal-05/soal-05.js
@@ -0,0 +1,11 @@
+/* Soal-05 */
+
+const harga = 18000;
+const jumlah = 6;
+const diskon = 10000;
+
+let subtotal = harga * jumlah
+let total = subtotal - diskon
+
+console.log(`Subtotal   : ${subtotal}`);
+console.log(`Total      : ${total}`);
