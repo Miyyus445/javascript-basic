b8746fa0bffee12e0ee3c71f8e71b1e3e4f0d08e
Miyyus.445 <miyyus.445@gmail.com>
2026-07-30 14:22:11 +0700

    Menyelesaikan Bab 5 - Soal 11

---
+++ bab-05-operator/soal/soal-11/soal-11.js
@@ -0,0 +1,25 @@
+/* Soal-11 */
+
+let angka = 20;
+
+angka + 5;
+
+console.log(angka);
+
+// Pertanyaan:                                  
+
+//1. Apa output program tersebut?               
+//2. Mengapa hasilnya demikian?                  
+//3. Perbaiki program agar menghasilkan: 25
+
+// Jawaban:
+
+//1. number
+//2. karena tidak memakai operator Assignment
+
+//3.
+let angka = 20;
+
+angka += 5;
+
+console.log(angka);
