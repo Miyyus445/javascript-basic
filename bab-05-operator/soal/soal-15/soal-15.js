f2ce2673ae9ac8a2b663e54e7e7a08d6744a33ff
Miyyus.445 <miyyus.445@gmail.com>
2026-07-30 14:51:39 +0700

    Menyelesaikan Bab 5 - Soal 15

---
+++ bab-05-operator/soal/soal-15/soal-15.js
@@ -0,0 +1,17 @@
+/* Soal-15 */
+
+let x = 12;
+
+x += 8;
+x *= 2;
+x--;
+x /= 3;
+
+console.log(x);
+
+// Awal : 12
+// Setelah += 8 : 20
+// Setelah *= 2 : 40
+// Setelah -- : 39
+// Setelah /= 3 : 13
+// Hasil akhir : 13
