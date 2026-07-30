6ce3f91cd2ce25f6ef610087beb29c599cf30629
Miyyus.445 <miyyus.445@gmail.com>
2026-07-30 14:46:13 +0700

    Menyelesaikan Bab 5 - Soal 14

---
+++ bab-05-operator/soal/soal-14/soal-14.js
@@ -0,0 +1,11 @@
+/* Soal-14 */
+
+const hargaBuku = 45000;
+const jumlahBeli = 4;
+const diskon = 15000;
+
+const subtotal = hargaBuku * jumlahBeli;
+const total = subtotal - diskon;
+
+console.log(`Subtotal   : ${subtotal}`);
+console.log(`Total      : ${total}`);
