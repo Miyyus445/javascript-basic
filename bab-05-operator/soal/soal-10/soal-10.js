6d8da2a4a10d4547f18c7da1b360dd53a123e8d1
Miyyus.445 <miyyus.445@gmail.com>
2026-07-30 14:10:27 +0700

    Menyelesaikan Bab 5 - Soal 10

---
+++ bab-05-operator/soal/soal-10/soal-10.js
@@ -0,0 +1,11 @@
+/* Soal-10 */
+
+console.log(8 + 4 * 2);
+console.log((8 + 4) * 2);
+console.log(20 / 5 + 3 * 2);
+
+// Prioritas
+
+//1. * lebih prioritas dibandingkan +
+//2. () lebih prioritas dibandingkan *
+//3. / lebih prioritas dibandingkan * karena javascript menghitung dari kiri ke kanan (sifat asosiatif)
