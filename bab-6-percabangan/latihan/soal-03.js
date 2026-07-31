c69791521c5384bc1a8789eb6ea198311f388bff
Miyyus.445 <miyyus.445@gmail.com>
2026-07-31 10:00:29 +0700

    Menyelesaikan Bab 6 - Soal 3

---
+++ bab-6-percabangan/latihan/soal-03.js
@@ -0,0 +1,16 @@
+/* Soal-03 */
+
+let nilai = 87;
+let predikat;
+
+if (nilai >= 90) {
+    console.log(predikat = 'A');
+} else if (nilai >= 80) {
+    console.log(predikat = 'B');
+} else if (nilai >= 70) {
+    console.log(predikat = 'C');
+} else if (nilai >= 60) {
+    console.log(predikat >= 'D');
+} else {
+    predikat = 'E'
+}
