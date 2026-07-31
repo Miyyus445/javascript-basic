1d7234011a669492013fc3d9684550d35579bed6
Miyyus.445 <miyyus.445@gmail.com>
2026-07-31 10:49:13 +0700

    Menyelesaikan Bab 6 - Soal 7

---
+++ bab-6-percabangan/latihan/soal-07.js
@@ -0,0 +1,13 @@
+/* Soal-07 */
+
+if (100) {
+    console.log("Aktif");
+}
+
+// Prediksi => Truthy
+
+// Pertanyaan :
+// Mengapa program tersebut tetap masuk ke dalam if?
+
+// Jawaban :
+// Karena if ada nilai nya, maka truthy 'Aktif'
