fc8e16d3aabc1853d08113bca946d7763d20e9a9
Miyyus.445 <miyyus.445@gmail.com>
2026-07-31 22:16:44 +0700

    Menyelesaikan Bab 6 - Soal 15

---
+++ bab-6-percabangan/latihan/soal-15.js
@@ -0,0 +1,10 @@
+/* Soal-15 */
+
+let username = "admin";
+let password = "12345";
+
+if (username && password) {
+    console.log('Login Berhasil');
+} else {
+    console.log('Username atau Password Salah');
+}
