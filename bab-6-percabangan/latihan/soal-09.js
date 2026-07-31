ddda97250c2d7e69103fd2e10110da100e06be62
Miyyus.445 <miyyus.445@gmail.com>
2026-07-31 13:38:42 +0700

    Menyelesaikan Bab 6 - Soal 9 & 10

---
+++ bab-6-percabangan/latihan/soal-09.js
@@ -0,0 +1,10 @@
+/* Soal-09 */
+
+let username = "budi";
+let password = "";
+
+if (username, password) {
+    console.log('Login Berhasil');
+} else {
+    console.log('Data Belum Lengkap');
+}
---
+++ bab-6-percabangan/latihan/soal-10.js
@@ -0,0 +1,13 @@
+/* Soal-10 */
+
+let warna = 'Merah';
+
+if (warna === 'Merah') {
+    console.log('BERHENTI!');
+} else if (warna === 'Kuning') {
+    console.log('Kurangi Kecepatan!');
+} else if (warna === 'Hijau') {
+    console.log('Jalan.');
+} else {
+    console.log('Lampu Tidak Di Kenal');
+}
