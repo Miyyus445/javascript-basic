b0d678a9b5cdd10fa972ea040902b7b8821b533f
Miyyus.445 <miyyus.445@gmail.com>
2026-07-31 22:28:39 +0700

    Menyelesaikan Bab 6 - Bonus

---
+++ bab-6-percabangan/latihan/bonus.js
@@ -0,0 +1,15 @@
+/* Bonus */
+
+let umur = 18;
+
+if (umur <= 5) {
+    console.log('Balita');
+} else if (umur <= 12) {
+    console.log('Anak - anak');
+} else if (umur <= 17) {
+    console.log('Remaja');
+} else if (umur <= 59) {
+    console.log('Dewasa');
+} else {
+    console.log('Lansia');
+}
