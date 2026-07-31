b482834b74287e2f771eb6e6aac0306fd628b36b
Miyyus.445 <miyyus.445@gmail.com>
2026-07-31 22:03:16 +0700

    Menyelesaikan Bab 6 - Soal 13

---
+++ bab-6-percabangan/latihan/soal-13.js
@@ -0,0 +1,13 @@
+/* Soal-13 */
+
+let nilai = 76;
+
+if (nilai >= 90) {
+    console.log('Sangat Baik');
+} else if (nilai >= 75) {
+    console.log('Baik');
+} else if (nilai >= 60) {
+    console.log('Cukup');
+} else if (nilai < 60) {
+    console.log('Perlu Belajar Lagi');
+}
