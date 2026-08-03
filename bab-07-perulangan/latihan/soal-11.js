f03529580c2b6dbcd77ec697a625d48ee21bc0d1
Miyyus.445 <miyyus.445@gmail.com>
2026-08-03 22:31:51 +0700

    Menyelesaikan Bab 7 - Soal 11

---
+++ bab-07-perulangan/latihan/soal-11.js
@@ -0,0 +1,10 @@
+/* Soal 11 - Challenge Bintang */
+
+for (let i = 1; i <= 5; i++){
+    let baris = '';
+    for (let j = 1; j <= i; j++) {
+        baris += '*';
+    }
+    console.log(baris);
+}
+
