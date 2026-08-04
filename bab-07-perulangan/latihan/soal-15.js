0d8b44ec9f4cd3969c1b6c0a62bdf72ebf5bfd2f
Miyyus.445 <miyyus.445@gmail.com>
2026-08-04 07:30:25 +0700

    Menyelesaikan Bab 7 - Soal 15

---
+++ bab-07-perulangan/latihan/soal-15.js
@@ -0,0 +1,8 @@
+/* Soal 15 - Challenge Akhir */
+
+const angkaPerkalian = 7;
+
+for (let i = 1; i <= 10; i++) {
+  let hasil = angkaPerkalian * i;
+  console.log(`${angkaPerkalian} x ${i} = ${hasil}`);
+}
