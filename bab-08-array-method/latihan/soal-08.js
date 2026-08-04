210796da9b2bf995ed5ad24bb6bae5295b39ab38
Miyyus.445 <miyyus.445@gmail.com>
2026-08-04 13:18:42 +0700

    Menyelesaikan Bab 8 - Soal 8

---
+++ bab-08-array-method/latihan/soal-08.js
@@ -0,0 +1,16 @@
+/* Soal-08 */
+
+let kota = [
+  "Jakarta",
+  "Bandung",
+  "Yogyakarta",
+  "Surabaya",
+];
+
+let bandung = kota.indexOf("Bandung")
+let surabaya = kota.indexOf("Surabaya")
+let bali = kota.indexOf("Bali")
+
+console.log(bandung);
+console.log(surabaya);
+console.log(bali);
