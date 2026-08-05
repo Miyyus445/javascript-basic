58abf42ec168e9a1e3485f303a1e13b3e90257f6
Miyyus.445 <miyyus.445@gmail.com>
2026-08-05 11:02:06 +0700

    Menyelesaikan Bab 9 - Soal 3

--- bab-09-fungsi/latihan/soal-03.js
+++ bab-09-fungsi/latihan/soal-03.js
@@ -2,7 +2,7 @@
 
 function tampilkanProduk (namaProduk, harga, stok) {
     console.log('Produk : ' + namaProduk);
-    console.log('Harga  : ' + harga);
+    console.log('Harga  : Rp.' + harga);
     console.log('Stok   : ' + stok);
 }
 
