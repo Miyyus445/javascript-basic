87571e24311c9a5d76aa55450ea0498f2775c970
Miyyus.445 <miyyus.445@gmail.com>
2026-08-05 16:31:24 +0700

    Menyelesaikan Bab 9 - Soal 12

---
+++ bab-09-fungsi/latihan/soal-12.js
@@ -0,0 +1,11 @@
+/* Soal-12 - Challenge Kalkulator*/
+
+const tambah = (a, b) => a + b;
+const kurang = (c, d) => c - d;
+const kali = (e, f) => e * f;
+const bagi = (g, h) => g / h;
+
+console.log('Tambah : ', tambah(7, 8));
+console.log('Kurang : ', kurang(10, 5));
+console.log('Kali   : ', kali(25, 2));
+console.log('Bagi   : ', bagi(8, 4));
