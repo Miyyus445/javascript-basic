a7244b223d5b643db7ecbcbff432abd3c7670311
Miyyus.445 <miyyus.445@gmail.com>
2026-08-05 10:31:12 +0700

    Menyelesaikan Bab 9 - Soal 2

---
+++ bab-09-fungsi/latihan/soal-02.js
@@ -0,0 +1,8 @@
+/* Soal-02 */
+
+function sayIntroduction (name, age) {
+    console.log('Halo Saya ' + name + '.');
+    console.log('Umur Saya ' + age + ' Tahun.');
+}
+
+sayIntroduction ('Azmi', 18)
