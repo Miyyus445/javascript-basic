a0771776a124e880124ed89f07add0da1ddb113e
Miyyus.445 <miyyus.445@gmail.com>
2026-08-05 21:30:35 +0700

    Menyelesaikan Bab 9 - Soal 13

--- bab-09-fungsi/latihan/soal-13.js
+++ bab-09-fungsi/latihan/soal-13.js
@@ -1,2 +1,16 @@
 /* Soal-13 - Challenge Menghitung Total Belanja */
 
+const hitungSubTotal = (harga, jumlah) => harga * jumlah;
+const hitungPPN = (subtotal) => (subtotal * 11) / 100;
+const hitungTotal = (subtotal, ppn) => subtotal + ppn;
+
+const harga = 25000;
+const jumlah = 4;
+
+const subtotal = hitungSubTotal(harga, jumlah);
+const ppn = hitungPPN(subtotal);
+const total = hitungTotal(subtotal, ppn);
+
+console.log('Subtotal : Rp', subtotal);
+console.log('PPN 11%  : Rp', ppn);
+console.log('Total    : Rp', total);
