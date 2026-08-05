195ab4c7aa5e8be1ab7b389e7c3baa8c8164da59
Miyyus.445 <miyyus.445@gmail.com>
2026-08-05 16:12:24 +0700

    Menyelesaikan Bab 9 - Soal 11

---
+++ bab-09-fungsi/latihan/soal-11.js
@@ -0,0 +1,11 @@
+/* Soal-11 - Challenge Biodata */
+
+function biodata (name, age, job) {
+    console.log('==== BIODATA ====');
+    console.log('Nama       : ' + name);
+    console.log('Umur       : ' + age);
+    console.log('Pekerjaan  : ' + job);
+    console.log('=================');
+}
+
+biodata ('Azmi', 18, 'Pelajar')
