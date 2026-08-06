a123dbed31c11f4f005a8bbb49ec0cd74e8dce58
Miyyus.445 <miyyus.445@gmail.com>
2026-08-06 10:46:10 +0700

    Menyelesaikan Bab 10 - Soal 1

---
+++ bab-10-object-method/latihan/soal-01.js
@@ -0,0 +1,11 @@
+/* Soal-01 */
+
+const mahasiswa = {
+    nama: 'Azmi',
+    umur: 18,
+    jurusan: 'Programer',
+    sudahLulus: 'masih masa pembelajaran',
+}
+
+console.log(mahasiswa);
+console.log(Object.entries(mahasiswa));
