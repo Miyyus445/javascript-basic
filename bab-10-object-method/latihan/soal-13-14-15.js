ab9c40782aa666b7255bc142c5aa30308b77563b
Miyyus.445 <miyyus.445@gmail.com>
2026-08-06 13:55:07 +0700

    Menyelesaikan Bab 10 - Soal 13, 14 ,15

---
+++ bab-10-object-method/latihan/soal-13-14-15.js
@@ -0,0 +1,11 @@
+/* Soal-13-14-15 */
+
+const smartphone = {
+  merek: "Samsung",
+  tipe: "S25",
+  warna: "Hitam",
+};
+
+console.log(Object.keys(smartphone));
+console.log(Object.values(smartphone));
+console.log(Object.entries(smartphone));
--- bab-10-object-method/latihan/soal-13.js
+++
@@ -1,9 +0,0 @@
-/* Soal-13 */
-
-const smartphone = {
-  merek: "Samsung",
-  tipe: "S25",
-  warna: "Hitam",
-};
-
-console.log(Object.keys(smartphone));
