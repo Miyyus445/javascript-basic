966aa3f5d2efeed6b043117d16eb93714d9b1dea
Miyyus.445 <miyyus.445@gmail.com>
2026-08-07 13:18:44 +0700

    Menyelesaikan Bab 11 - Soal 9

---
+++ bab-11-iterable-dan-non-iterable/latihan/soal-09.js
@@ -0,0 +1,12 @@
+/* Soal-09 */
+
+// Data:    Gunakan:
+
+// Array    Iterable     (for...of)
+// String   Iterable     (for...of)
+// Object   Non-Iterable (for...in)
+
+// Alasan:
+
+// Iterable => Data yang punya urutan dari depan ke belakang.
+// Non-Iterable => Data tunggal yang ga ada urutan index.
