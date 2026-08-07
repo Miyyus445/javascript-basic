567e05071bd67961bf01d6f628a275c8fa96483f
Miyyus.445 <miyyus.445@gmail.com>
2026-08-07 10:35:07 +0700

    Menyelesaikan Bab 11 - Soal 1

---
+++ bab-11-iterable-dan-non-iterable/latihan/soal-01.js
@@ -0,0 +1,17 @@
+/* Soal-01 */
+
+//1. "Indonesia"
+//2. ["Merah", "Putih"]
+//3. 150
+//4. true
+//5. { nama: "Budi" }
+
+
+// Jawaban:             Alasan:
+
+//1. Iterable           Iterable,
+//2. Iterable           Data yang punya urutan dari depan ke belakang.
+
+//3. Non-Iterable       Non-Iterable,
+//4. Non-Iterable       Data tunggal
+//5. Non-Iterable       yang ga ada urutan index.
