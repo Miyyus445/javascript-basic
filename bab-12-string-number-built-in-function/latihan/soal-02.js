d8b892d4a5eb485122acf78577f89f0e37cc8794
Miyyus.445 <miyyus.445@gmail.com>
2026-08-10 11:21:24 +0700

    Menyelesaikan Bab 12 - Soal 2

---
+++ bab-12-string-number-built-in-function/latihan/soal-02.js
@@ -0,0 +1,9 @@
+/* Soal 2 */
+
+const email = "   Budi@Email.COM   ";
+
+const emailTrimmed = email.trim();
+const emailLowerCase = emailTrimmed.toLowerCase();
+
+console.log(emailTrimmed);
+console.log(emailLowerCase);
