91dc4ed16edc91f5e21778c47d08b37e09772473
Miyyus.445 <miyyus.445@gmail.com>
2026-08-10 13:48:15 +0700

    Menyelesaikan Bab 12 - Soal 3

---
+++ bab-12-string-number-built-in-function/latihan/soal-03.js
@@ -0,0 +1,12 @@
+/* Soal 3 */
+
+const username = "santri_jago_ngoding";
+
+const jumlahKarakter = username.length;
+if (jumlahKarakter < 8) {
+    console.log("Username terlalu pendek");
+} else {
+    console.log("Username valid");
+}
+
+console.log(jumlahKarakter);
