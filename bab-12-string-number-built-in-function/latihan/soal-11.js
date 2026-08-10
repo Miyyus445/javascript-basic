ace2d5ec8366934f15207434dba4fc97e8583b06
Miyyus.445 <miyyus.445@gmail.com>
2026-08-10 14:29:14 +0700

    Menyelesaikan Bab 12 - Soal 11

---
+++ bab-12-string-number-built-in-function/latihan/soal-11.js
@@ -0,0 +1,11 @@
+/* Soal 11 */
+
+const nilai1 = "85";
+const nilai2 = "90";
+
+console.log(nilai1 + nilai2); // Output: 8590
+
+const nilai1Number = Number(nilai1);
+const nilai2Number = Number(nilai2);
+
+console.log(`Total Nilai: ${nilai1Number + nilai2Number}`); // Output: Total Nilai: 175
