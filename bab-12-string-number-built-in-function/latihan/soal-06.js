b6cee4cf2f1b5621d5cbdbe6df5aaf93fbd809b8
Miyyus.445 <miyyus.445@gmail.com>
2026-08-10 14:08:57 +0700

    Menyelesaikan Bab 12 - Soal 6

--- bab-12-string-number-built-in-function/latihan/soal-06.js
+++ bab-12-string-number-built-in-function/latihan/soal-06.js
@@ -2,3 +2,8 @@
 
 const url = "https://santri.dev";
 
+const cekAwalan = url.startsWith("https");
+const cekAkhiran = url.endsWith(".dev");
+
+console.log(`Menggunakan HTTPS: ${ cekAwalan }`);
+console.log(`domain .dev: ${ cekAkhiran }`);
