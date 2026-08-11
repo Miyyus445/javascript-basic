caa91b8cd9541a0ad53964aa1e387df8db2abf95
Miyyus.445 <miyyus.445@gmail.com>
2026-08-12 06:25:42 +0700

    Menyelesaikan Bab 13 - Soal 11

---
+++ bab-13-scope-hoisting-closure/latihan/soal-11.js
@@ -0,0 +1,28 @@
+/* Soal-11 */
+
+// Kode berikut menghasilkan error:
+
+// function tampilkanData() {
+//   console.log(nama);
+//   console.log(umur);
+// }
+
+// tampilkanData();
+
+// const nama = "Budi";
+// const umur = 17;
+
+// Perbaiki kode tersebut agar dapat dijalankan dengan benar.
+
+const nama = "Budi";
+const umur = 17;
+
+function tampilkanData() {
+  console.log(nama);
+  console.log(umur);
+}
+
+tampilkanData();
+
+// Output: Budi
+//         17
