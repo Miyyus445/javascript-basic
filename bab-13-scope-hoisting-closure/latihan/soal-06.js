ddec2845a49be3520cdce24439db3b6140ad6bae
Miyyus.445 <miyyus.445@gmail.com>
2026-08-12 05:42:30 +0700

    Menyelesaikan Bab 13 - Soal 6

---
+++ bab-13-scope-hoisting-closure/latihan/soal-06.js
@@ -0,0 +1,10 @@
+/* Soal-06 */
+
+sapa();
+
+function sapa() {
+  console.log("Halo Santri!");
+}
+
+// Mengapa fungsi dapat dipanggil sebelum deklarasinya?
+// - karena function dapat di deklarasikan dengan hoisting function
