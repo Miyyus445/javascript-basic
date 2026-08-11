e47727ac72ab14447a2026b650e786cdbed0bf12
Miyyus.445 <miyyus.445@gmail.com>
2026-08-11 22:09:33 +0700

    Menyelesaikan Bab 13 - Soal 2

---
+++ bab-13-scope-hoisting-closure/latihan/soal-02.js
@@ -0,0 +1,18 @@
+/* Soal-02 */
+
+const nama = "Budi";
+
+function tampilkanNama() {
+  const umur = 17;
+
+  console.log(nama);
+  console.log(umur);
+}
+
+tampilkanNama();
+
+console.log(nama);
+console.log(umur);
+
+// Baris mana yang berhasil dijalankan?     -function tampilkanNama()
+// Baris mana yang menghasilkan error?      -console.log(umur);
