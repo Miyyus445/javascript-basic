0c38f897f4d323fe6a0926d0bdf2a9ac34c6f2d6
Miyyus.445 <miyyus.445@gmail.com>
2026-08-12 06:02:44 +0700

    Menyelesaikan Bab 13 - Soal 8

---
+++ bab-13-scope-hoisting-closure/latihan/soal-08.js
@@ -0,0 +1,22 @@
+/* Soal-08 */
+
+console.log(nama);
+
+let nama = "Budi";
+
+// Apakah hasilnya:
+
+// A.
+// undefined
+
+// B.
+// Budi
+
+// C.
+// ReferenceError
+
+
+// Pilih jawaban dan jelaskan alasannya.
+
+// Jawabannya (C. ReferenceError)
+// Karena variabel let/const tidak dapat di hoisting
