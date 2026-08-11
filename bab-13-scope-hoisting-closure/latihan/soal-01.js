c31ef369523d762c929059ad99b5e8a83050f448
Miyyus.445 <miyyus.445@gmail.com>
2026-08-11 22:05:53 +0700

    Menyelesaikan Bab 13 - Soal 1

---
+++ bab-13-scope-hoisting-closure/latihan/soal-01.js
@@ -0,0 +1,16 @@
+/* Soal-01 */
+
+const nama = "Budi";
+
+function belajar() {
+  const bahasa = "JavaScript";
+
+  if (true) {
+    let level = "Pemula";
+    console.log(level);
+  }
+}
+
+// nama termasuk scope apa?     - Global Scope
+// bahasa termasuk scope apa?   - Function Scope
+// level termasuk scope apa?    - Block Scope
