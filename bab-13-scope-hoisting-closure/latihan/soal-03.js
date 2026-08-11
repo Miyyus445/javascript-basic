b52834ba2f8d1758b7ea11b917393499518d688f
Miyyus.445 <miyyus.445@gmail.com>
2026-08-11 22:12:45 +0700

    Menyelesaikan Bab 13 - Soal 3

---
+++ bab-13-scope-hoisting-closure/latihan/soal-03.js
@@ -0,0 +1,13 @@
+/* Soal-03 */
+
+if (true) {
+  let pesan = "Halo Santri";
+
+  console.log(pesan);
+}
+
+console.log(pesan);
+
+// Apakah program berhasil?             - Tidak
+
+// Jika tidak, jelaskan penyebabnya.    - karena variabel pesan di dalam block scope
