c9045dab099cf6e105c7220ad7ca25995e9ebbd2
Miyyus.445 <miyyus.445@gmail.com>
2026-08-12 17:29:45 +0700

    Menyelesaikan Bab 13 - Study Case 3

--- bab-13-scope-hoisting-closure/latihan/study-case-03.js
+++ bab-13-scope-hoisting-closure/latihan/study-case-03.js
@@ -1,2 +1,36 @@
 /* Study Case 03 */
 
+function buatDompet(saldoAwal) {
+  let saldo = saldoAwal;
+  return {
+    cekSaldo() {
+      console.log("Saldo Awal:", saldo);
+    },
+
+    tambahSaldo(jumlahSetor) {
+
+      if (jumlahSetor > 0) {
+        saldo += jumlahSetor;
+        console.log("Setor berhasil! Saldo sekarang:", saldo);
+      } else {
+        console.log("Jumlah setor harus lebih dari 0!");
+      }
+    },
+
+    kurangiSaldo(jumlahTarik) {
+      if (jumlahTarik <= saldo) {
+        saldo -= jumlahTarik;
+        console.log("Penarikan berhasil! Saldo tersisa:", saldo);
+      } else {
+        console.log("Saldo tidak cukup!");
+      }
+    }
+  };
+}
+
+const dompet = buatDompet(50000); // Saldo awal 50000
+
+dompet.cekSaldo();
+dompet.tambahSaldo(25000);
+dompet.kurangiSaldo(10000);
+dompet.cekSaldo();
