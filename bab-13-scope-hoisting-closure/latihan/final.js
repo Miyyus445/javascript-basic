b8b60d4c8b6ac94d0c189fd35e71c711905dfe1f
Miyyus.445 <miyyus.445@gmail.com>
2026-08-12 21:56:43 +0700

    Menyelesaikan Bab 13 - FINAL

---
+++ bab-13-scope-hoisting-closure/latihan/final.js
@@ -0,0 +1,50 @@
+/* FINAL */
+
+function buatAkun(nama, saldoAwal) {
+  let saldo = saldoAwal;
+
+  return {
+    lihatProfil() {
+      console.log("===== PROFIL =====");
+      console.log(`Nama: ${nama}\n`);
+    },
+
+    cekSaldo() {
+      console.log(`Saldo: Rp${saldo}\n`);
+    },
+
+    tambahSaldo(jumlah) {
+      saldo += jumlah;
+      console.log("Saldo berhasil ditambahkan.");
+      console.log(`Saldo sekarang: Rp${saldo}\n`);
+    },
+
+    kurangiSaldo(jumlah) {
+      if (jumlah <= saldo) {
+        saldo -= jumlah;
+        console.log("Saldo berhasil dikurangi.");
+        console.log(`Saldo sekarang: Rp${saldo}\n`); 
+      } else {
+        console.log("Saldo tidak mencukupi.\n");
+      }
+    }
+  };
+}
+
+const akunBudi = buatAkun("Budi", 100000);
+const akunAni = buatAkun("Ani", 500000);
+
+// === Akun Budi ===
+akunBudi.lihatProfil();
+akunBudi.cekSaldo();
+akunBudi.tambahSaldo(50000);
+akunBudi.kurangiSaldo(25000);
+akunBudi.cekSaldo();
+
+console.log("-------------------\n");
+
+// === Akun Ani ===
+akunAni.lihatProfil();
+akunAni.cekSaldo();
+akunAni.tambahSaldo(100000);
+akunAni.kurangiSaldo(700000);
