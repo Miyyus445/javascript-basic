0d1f85f021611828b2d373d4487ea9956d7fe0f0
Miyyus.445 <miyyus.445@gmail.com>
2026-08-13 11:29:53 +0700

    Menyelesaikan Bab 14 - Soal 2

---
+++ bab-14-date-object/tugas32/soal-02.js
@@ -0,0 +1,20 @@
+/* Soal-02 */
+
+const sekarang = new Date();
+
+const tahun = sekarang.getFullYear();
+const bulan = sekarang.getMonth() + 1;
+const tanggal = sekarang.getDate();
+const hari = sekarang.getDay();
+const jam = sekarang.getHours();
+const menit = sekarang.getMinutes();
+const detik = sekarang.getSeconds();
+
+console.log("Waktu Sekarang:");
+console.log(`Tahun  : ${tahun}`);
+console.log(`Bulan  : ${bulan}`);
+console.log(`Tanggal: ${tanggal}`);
+console.log(`Hari   : ${hari}`);
+console.log(`Jam    : ${jam}`);
+console.log(`Menit  : ${menit}`);
+console.log(`Detik  : ${detik}`);
