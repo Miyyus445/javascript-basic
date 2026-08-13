790b4d9cad29e504eaec4a90970f29c10bcec445
Miyyus.445 <miyyus.445@gmail.com>
2026-08-13 11:22:34 +0700

    Menyelesaikan Bab 14 - Soal 1

--- bab-14-date-object/tugas32/soal-01.js
+++ bab-14-date-object/tugas32/soal-01.js
@@ -3,4 +3,4 @@
 const tanggal = new Date();
 
 console.log("Waktu Sekarang:");
-console.log(tanggal.toString()); // Output: Thu Aug 13 2026 11:20:18 GMT+0700 (Western Indonesia Time
+console.log(tanggal.toString()); // Output: Thu Aug 13 2026 11:20:18 GMT+0700 (Western Indonesia Time)
