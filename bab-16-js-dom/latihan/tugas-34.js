ba25d2e14e438a5a101fb1e3244b7dc09d92e592
Miyyus.445 <miyyus.445@gmail.com>
2026-08-19 13:53:20 +0700

    Menambahkan File JS

---
+++ bab-16-js-dom/latihan/tugas-34.js
@@ -0,0 +1,12 @@
+const santri = {
+  nama: "Budi Santoso",
+  kelas: "XI RPL",
+  status: "Aktif"
+};
+
+const aktivitas = [
+  "Login ke aplikasi",
+  "Membaca materi JavaScript",
+  "Mengerjakan latihan",
+  "Mengumpulkan tugas"
+];
