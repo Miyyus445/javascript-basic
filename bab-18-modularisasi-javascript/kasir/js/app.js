939c21a3a73e9aab9c5cf93cc08b27338eaa36e6
Miyyus.445 <miyyus.445@gmail.com>
2026-08-24 14:48:11 +0700

    Menambahkan File app.JS

---
+++ bab-18-modularisasi-javascript/kasir/js/app.js
@@ -0,0 +1,68 @@
+/* App */
+
+import { produk, cariProduk } from "./produk.js";
+
+import formatRupiah, { hitungSubtotal, hitungTotal } from "./transaksi.js";
+
+const listProdukConatiner = document.querySelector("#daftar-produk-list");
+const strukContainer = document.querySelector("#struk-transaksi");
+const totalBiayaContainer = document.querySelector("#total-biaya");
+
+function renderDaftarProduk() {
+    listProdukConatiner.innerHTML = "";
+
+    produk.forEach((item, index) => {
+        const li = document.createElement("li");
+        li.className = "produk-item";
+        li.innerHTML = `
+        <span>${index + 1}. <strong>${item.nama}</strong></span>
+        <span class="harga">${formatRupiah(item.harga)}</span>
+        `;
+        listProdukConatiner.appendChild(li);
+    });
+}
+
+function jalankanSimulasiTransaksi() {
+    const keranjang = [
+        { idProduk: 1, jumlah: 2 },
+        { idProduk: 2, jumlah: 1 },
+        { idProduk: 3, jumlah: 1 },
+    ];
+
+    const daftarStruk = [];
+
+    strukContainer.innerHTML = "";
+
+    keranjang.forEach((item) => {
+        const dataProduk = cariProduk(item.idProduk);
+
+        if (dataProduk) {
+            const subTotal = hitungSubtotal(dataProduk.harga.item.jumlah);
+
+            daftarStruk.push({
+                nama: dataProduk.nama,
+                harga: dataProduk.harga,
+                jumlah: item.jumlah,
+                subtotal: subTotal
+            });
+
+            const itemStruk = document.createElement("div");
+            itemStruk.className = "struk-item";
+            itemStruk.innerHTML = `
+                <div class="struk-detail">
+                    <p><strong>${dataProduk.nama}</strong></p>
+                    <p class="sub-text">${formatRupiah(dataProduk.harga)} x ${item.jumlah}</p>
+                </div>
+                <div class="struk-subtotal">${formatRupiah(subtotal)}</div>
+            `;
+
+            strukContainer.appendChild(itemStruk);
+        }
+    });
+
+    const grandTotal = hitungTotal(daftarStruk);
+    totalBiayaContainer.textContent = formatRupiah(grandTotal);
+}
+
+renderDaftarProduk();
+jalankanSimulasiTransaksi();
