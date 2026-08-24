3319bc0ad1cb0d27b60d55a788dac49670ae054c
Miyyus.445 <miyyus.445@gmail.com>
2026-08-24 11:13:25 +0700

    Menambahkan File Produk.JS

---
+++ bab-18-modularisasi-javascript/kasir/js/produk.js
@@ -0,0 +1,19 @@
+/* Data Produk */
+
+// named export
+export const produk = [
+    {
+        id: 1,
+        nama: "Keyboard",
+        harga: 250000,
+    },
+    {
+        id: 2,
+        nama: "Mouse",
+        harga: 150000,
+    },
+    {
+        id: 3,
+        nam
+    }
+]
