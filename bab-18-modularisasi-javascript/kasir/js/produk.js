55011035c00b9579ff353bb570de710416f7c389
Miyyus.445 <miyyus.445@gmail.com>
2026-08-24 11:17:07 +0700

    Update File Produk.JS

--- bab-18-modularisasi-javascript/kasir/js/produk.js
+++ bab-18-modularisasi-javascript/kasir/js/produk.js
@@ -2,18 +2,23 @@
 
 // named export
 export const produk = [
-    {
-        id: 1,
-        nama: "Keyboard",
-        harga: 250000,
-    },
-    {
-        id: 2,
-        nama: "Mouse",
-        harga: 150000,
-    },
-    {
-        id: 3,
-        nam
-    }
-]
+  {
+    id: 1,
+    nama: "Keyboard",
+    harga: 250000,
+  },
+  {
+    id: 2,
+    nama: "Mouse",
+    harga: 150000,
+  },
+  {
+    id: 3,
+    nama: "Headset",
+    harga: 300000,
+  },
+];
+
+export function cariProduk(id) {
+    return produk.find((item) => item.id === id);
+};
